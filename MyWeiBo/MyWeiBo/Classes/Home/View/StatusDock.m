110e309e92a0ac40a482563b4d8f641eabe531da
ZYmac <liuchongxin@liuchongxindeiMac.local>
2014-09-22 13:15:08 +0800

    status按钮添加

--- MyWeiBo/MyWeiBo/Classes/Home/View/StatusDock.m
+++ MyWeiBo/MyWeiBo/Classes/Home/View/StatusDock.m
@@ -24,20 +24,27 @@
     if (self) {
         self.backgroundColor = [UIColor whiteColor];
         
-        UIButton *repost = [[UIButton alloc]init];
-        [repost setTitle:@"转发" forState:UIControlStateNormal];
-        [repost setTitleColor:kColor(220, 220, 220) forState:UIControlStateNormal];
-        repost.titleLabel.font = kTextFont;
-        [repost setImage:[UIImage imageNamed:@"timeline_icon_retweet"] forState:UIControlStateNormal];
-        CGFloat btnW = [UIScreen mainScreen].bounds.size.width / 3;
-        repost.frame = (CGRect){{0,0},{btnW,kStatusDockHeight}};
-        [self addSubview:repost];
-        _repost = repost;
+        _repost = [self addButtonWithTitle:@"转发" imageName:@"timeline_icon_retweet" index:0];
+        _comment = [self addButtonWithTitle:@"评论" imageName:@"timeline_icon_comment" index:1];
+        _attitute = [self addButtonWithTitle:@"赞" imageName:@"timeline_icon_unlike" index:2];
         
     }
     return self;
 }
 
+-(UIButton *)addButtonWithTitle:(NSString *)title imageName:(NSString *)imageName index:(NSInteger)index{
+    
+    UIButton *btn = [[UIButton alloc]init];
+    [btn setTitle:title forState:UIControlStateNormal];
+    [btn setTitleColor:kColor(100, 100, 100) forState:UIControlStateNormal];
+    btn.titleLabel.font = kTextFont;
+    btn.titleEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0);
+    [btn setImage:[UIImage imageNamed:imageName] forState:UIControlStateNormal];
+    CGFloat btnW = [UIScreen mainScreen].bounds.size.width / 3;
+    btn.frame = (CGRect){{btnW * index,0},{btnW,kStatusDockHeight}};
+    [self addSubview:btn];
 
+    return btn;
+}
 
 @end
